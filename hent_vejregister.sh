cd1b3adf5dfe718cc5fc3eadfb0bd4099c306815
Mads <mads.eckardt@gmail.com>
2023-06-14 14:53:19 +0200

    Update hent_vejregister.sh

--- hent_vejregister.sh
+++ hent_vejregister.sh
@@ -2,11 +2,20 @@
 clear
 ZIP_FILENAME="vejregister.zip"
 URL="https://cpr.dk/kunder/gratis-download/vejregister-og-udtraeksbeskrivelse"
-FILE_URI=$(wget -q -O - $URL | grep -o -E "/Media.*?"$(date "+%y")".*?\.zip" | sed 's/^/https:\/\/cpr.dk\//')
+HTML=$(wget -q -O - $URL)
+FILE_URI=
+case $OSTYPE in
+  darwin*)
+    FILE_URI=$(echo $HTML | grep -o -E "/Media.*?"$(date "+%y")".*?\.zip" | head -n 1 | sed 's/^/https:\/\/cpr.dk\//')
+  ;;
+  linux*)
+    FILE_URI=$(echo $HTML | grep -o -P "/Media.*?"$(date "+%y")".*?\.zip" head -n 1 | sed 's/^/https:\/\/cpr.dk\//')
+  ;;
+esac
 
 if [[ ! -z "$FILE_URI" ]]
 then
-  echo "1. Henter $FILE_URI ($HOME/$ZIP_FILENAME)."
+  echo "1. Henter $FILE_URI ($HOME/$ZIP_FILENAME)"
   rm -f $HOME/$ZIP_FILENAME
   wget -q $FILE_URI -O $HOME/$ZIP_FILENAME
   FILENAME=$(zipinfo -1 $HOME/$ZIP_FILENAME)
@@ -20,8 +29,8 @@ then
   tput sgr0
   echo "  1. Tjek at datostemplet på .zip-filen rigtigt ud."
   echo "  2. HUSK TMUX!"
-  echo "  3. Indlæs filen med '/app/cprmirror/cprdbimport/cprdbimport.rb -t vejregister -o import -e -f $(realpath $HOME/$FILENAME)'"
-  echo "  4. Evt. slet $HOME/$FILENAME"
+  echo "  3. Indlæs filen med '/app/cprmirror/cprdbimport/cprdbimport.rb -t vejregister -o import -e -f $(realpath $HOME/$FILENAME)'."
+  echo "  4. Slet $HOME/$FILENAME når du er færdig."
 else
   echo "Kunne ikke finde filen :-("
   exit 1
